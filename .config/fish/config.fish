1ce20def4b172132d1874dfa0d67a008358355c8
Gert Leenders <leenders.gert@gmail.com>
2015-09-12 20:48:13 +0200

    Fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,29 @@
 set -g -x PATH /usr/local/bin $PATH
+
+#h git prompt
+#
+#
+#
+#
+set -g __fish_git_prompt_showupstream informative verbose
+#set -g __fish_git_prompt_show_informative_status 1
+#set -g __fish_git_prompt_showupstream 1
+set -g __fish_git_prompt_showuntrackedfiles 1
+#set -g __fish_git_prompt_showdirtystate 1
+#set -g __fish_git_prompt_showstashstate 1
+#set -g __fish_git_prompt_showcolorhints 1
+#
+#
+#
+#set __fish_git_prompt_showupstream 'yes'
+#set __fish_git_prompt_color_branch yellow
+#set __fish_git_prompt_color_upstream_ahead green
+#set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+#set __fish_git_prompt_char_dirtystate '⚡'
+#set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '  ☡'
+#set __fish_git_prompt_char_stashstate '↩'
+#set __fish_git_prompt_char_upstream_ahead '+'
+#set __fish_git_prompt_char_upstream_behind '-'
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,9 @@ mkdir -p ~/.config/fish
 
 
 chsh -s /usr/local/bin/fish
+
+
+git clone https://github.com/milkbikis/powerline-shell
+./install.py
+
+chmod +x powerline-shell.py
