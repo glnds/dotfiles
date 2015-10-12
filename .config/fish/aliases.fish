72e1d01c7668493c72a87585b3d18df1cd4d72ac
Gert Leenders <leenders.gert@gmail.com>
2015-10-12 15:08:17 +0200

    new alias

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -92,6 +92,7 @@ alias pfa="p4 add"
 alias pfA="find . -type f -print | p4 -x - add"
 alias pfsm="p4 submit"
 alias pfo="p4 opened"
+alias pfd="p4 delete"
 function pfe
   p4 edit $argv ; vim $argv
 end
