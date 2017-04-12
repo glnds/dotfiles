8e5d19f00b179c830668436e23c65856257abc6c
Gert Leenders <leenders.gert@gmail.com>
2017-04-12 13:24:48 +0200

    Added aws cli autompletion

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -60,4 +60,7 @@ else
     end
 end
 
+# Activate AWS cli auto completion
+complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+
 #archey -c
