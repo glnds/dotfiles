74f832e1aee06d0b7bb54ccfb5af26de56b15828
Gert Leenders <leenders.gert@gmail.com>
2017-05-26 14:48:03 +0200

    Added saw ssh key to identities

--- .config/fish/functions/start_agent.fish
+++ .config/fish/functions/start_agent.fish
@@ -8,4 +8,5 @@ function start_agent
   . $SSH_ENV > /dev/null
   #ssh-add ~/.ssh/(hostname)
   ssh-add ~/.ssh/mackey
+  ssh-add  ~/.ssh/saw_key
 end
--- .config/fish/functions/test_identities.fish
+++ .config/fish/functions/test_identities.fish
@@ -2,7 +2,6 @@
 function test_identities
   ssh-add -l | grep "The agent has no identities" > /dev/null
   if [ $status -eq 0 ]
-    #ssh-add ~/.ssh/(hostname)
     ssh-add ~/.ssh/mackey
     ssh-add ~/.ssh/saw_key
     if [ $status -eq 2 ]
