58806de6c301758c4473405eb8840179a09a2e94
Gert Leenders <leenders.gert@gmail.com>
2017-10-02 12:55:11 +0200

    Fixed fish function error

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,26 +1,8 @@
 function fish_right_prompt -d "Write out the right prompt"
 
-  # set_color black -b f9a563
-  # set_color -o f48f41
   set_color -o ffa500
   echo "<"
   echo $AWS_PROFILE
   echo ">"
   set_color normal
-
-  # Print a yellow fork symbol when in a subshell
-  set -l max_shlvl 1
-  test $TERM = "screen"; set -l max_shlvl 2
-  if test $SHLVL -gt $max_shlvl
-    set_color yellow
-    echo -n " ⑂"
-    set_color normal
-  end
-
-  # Print a red dot for failed commands.
-  if test $exit_code -ne 0
-    set_color red
-    echo -n " •"
-    set_color normal
-  end
 end
