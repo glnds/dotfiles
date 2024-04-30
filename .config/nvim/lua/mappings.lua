23f67f4b2c0d8d0aa4a65dfb1ced8b5d2a7f477c
Gert Leenders <leenders.gert@gmail.com>
2024-04-30 11:31:57 +0200

    changed vim config to NvChad

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,13 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+-- `jj` is much better :)
+map("i", "jj", "<ESC>")
+-- Visual mode
+map("n", "<Leader><Leader>", "V|")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
