155414f2b613c63aed014c531f40241f6ecca51f
Gert Leenders <leenders.gert@gmail.com>
2024-05-14 09:08:14 +0200

    Cleanup fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,11 +15,10 @@ set -g -x PAGER less
 # fish_vi_mode
 set fish_key_bindings fish_user_key_bindings
 
-
 # git prompt
 # https://github.com/fish-shell/fish-shell/blob/master/share/functions/__fish_git_prompt.fish
 set -g __fish_git_prompt_showupstream verbose
-#set -g __fish_git_prompt_show_informative_status 1
+# set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_showuntrackedfiles 1
 set -g __fish_git_prompt_showdirtystate 1
 set -g __fish_git_prompt_showstashstate 1
