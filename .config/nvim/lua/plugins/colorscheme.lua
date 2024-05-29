507ce0997ee92324e6068ae3166d6f9a5de0e85f
Gert Leenders <leenders.gert@gmail.com>
2024-05-29 13:35:38 +0200

    colorscheme update

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 return {
   -- { "ellisonleao/gruvbox.nvim", lazy = false, priority = 1000 },
-  -- { "sjl/badwolf", lazy = false, priority = 1000 },
+  { "sjl/badwolf", lazy = false, priority = 1000 },
   -- { "polirritmico/monokai-nightasty.nvim", lazy = false, priority = 1000 },
   { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
   -- { "ribru17/bamboo.nvim", lazy = false, priority = 1000 },
@@ -9,7 +9,8 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "moonfly",
+      -- colorscheme = "moonfly",
+      colorscheme = "badwolf",
     },
   },
 }
