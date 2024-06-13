a34a0264937ad3a5577f8e2217f6c056fa3e0b4e
Gert Leenders <leenders.gert@gmail.com>
2024-06-13 10:25:10 +0200

    Updates

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,26 +1,26 @@
 function fish_prompt --description 'Write out the prompt'
-  if set -q VIRTUAL_ENV
-    echo -n -s (set_color -o black -b brcyan) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
-  end
+    if set -q VIRTUAL_ENV
+        echo -n -s (set_color -b blue white) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+    end
 
-	set -l last_status $status
+    set -l last_status $status
 
-	if not set -q __fish_prompt_normal
-    set -g __fish_prompt_normal (set_color normal)
-	end
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
 
-	# PWD
-	set_color $fish_color_cwd
-	echo -n (prompt_pwd)
-	set_color normal
+    # PWD
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
 
-	printf '%s ' (__fish_git_prompt)
+    printf '%s ' (__fish_git_prompt)
 
-	if not test $last_status -eq 0
-	set_color $fish_color_error
-	end
+    if not test $last_status -eq 0
+        set_color $fish_color_error
+    end
 
-	echo -n '$ '
+    echo -n '$ '
 
-	set_color normal
+    set_color normal
 end
