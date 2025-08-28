c4b0c1ed742551a14fc55fa0b124e12e51a12f9e
Gert Leenders <leenders.gert@gmail.com>
2025-08-28 20:00:29 +0200

    changed colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,19 @@
 return {
-  -- { "ellisonleao/gruvbox.nvim", lazy = false, priority = 1000 },
+  { "rebelot/kanagawa.nvim", lazy = false, priority = 1000 },
   { "sjl/badwolf", lazy = false, priority = 1000 },
-  -- { "polirritmico/monokai-nightasty.nvim", lazy = false, priority = 1000 },
+  { "EdenEast/nightfox.nvim", lazy = false, priority = 1000 },
   { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
-  -- { "ribru17/bamboo.nvim", lazy = false, priority = 1000 },
+  { "morhetz/gruvbox", lazy = false, priority = 1000 },
+  { "sainnhe/sonokai", lazy = false, priority = 1000 },
 
   -- Configure LazyVim to load colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "moonfly",
-      colorscheme = "badwolf",
+      colorscheme = "gruvbox",
+      -- colorscheme = "sonokai",
+      -- colorscheme = "nightfox",
+      -- colorscheme = "kanagawa",
     },
   },
 }
