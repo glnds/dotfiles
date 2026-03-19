2d3fe95485c4a19cd9a9e9281020e6bdf2248c7c
Gert Leenders <leenders.gert@gmail.com>
2026-03-19 08:46:46 +0100

    fish: lighten git prompt, rm dead kiro/fig startup files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,11 +19,10 @@ set fish_key_bindings fish_user_key_bindings
 
 # git prompt
 # https://github.com/fish-shell/fish-shell/blob/master/share/functions/__fish_git_prompt.fish
-set -g __fish_git_prompt_showupstream verbose
+set -g __fish_git_prompt_showupstream auto
 # set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_showuntrackedfiles 1
 set -g __fish_git_prompt_showdirtystate 1
-set -g __fish_git_prompt_showstashstate 1
 set -g __fish_git_prompt_showcolorhints 1
 
 set __fish_git_prompt_color_branch yellow
